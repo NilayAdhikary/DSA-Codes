ab870da311369d2097bc95a034710d102a9e0451
Nilay Adhikary <adhikarinilay@gmail.com>
2024-07-31 14:44:35 +0530

    new changes

--- Sorting/SelectionSort.java
+++ Sorting/SelectionSort.java
@@ -2,9 +2,20 @@ package Sorting;
 
 public class SelectionSort {
     private static void doSelectionSort(int[] arr, int n) {
+        
+        // T.C.-->
+        // inner loop runs 0 to n-1(n times)
+        // Then 1 to n-1(n-1 times)
+        // Then 2 to n-1(n-2 times)....
+        // ...........
+        // At last it runs n-2 to n-1(1 time)
+        // n+(n-1)+(n-2)+...+2+1
+        // (n*(n+1))/2 i.e. O(n^2);
+        
         for(int i = 0; i<n-1; i++){
             int minIndex = i; //My minimum number appears at index i after every iteration of i;
             
+
             for(int j = i; j<n; j++){
                 if(arr[j] < arr[minIndex]){
                     minIndex = j;
